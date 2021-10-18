c40872a556743008b63a2bb5d49818b57088e01c
татьяна <emuart155@gmail.com>
2021-10-18 20:15:20 +0300

    Добавила список и ViewModel с нашими методами Dao в NotesListFragment.kt

--- app/src/main/java/com/example/notes/NotesAdapter.kt
+++ app/src/main/java/com/example/notes/NotesAdapter.kt
@@ -1,40 +1,78 @@
 package com.example.notes
 
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import com.example.notes.databinding.FragmentNotesListBinding
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.notes.databinding.ItemNoteBinding
+import com.example.notes.room.NoteEntity
+import com.example.notes.room.getFormattedData
 
-/**
- * [RecyclerView.Adapter] that can display a [PlaceholderItem].
- * TODO: Replace the implementation with code for your data type.
- */
-class NotesAdapter(
-    private val values: List<NotesAdapter>
-) : RecyclerView.Adapter<NotesAdapter.ViewHolder>() {
+//см 5.1.2.8
+//Функция onItemClicked будет использоваться для обработки навигации, когда элемент выбран на первом экране,
+//а для второго экрана вы просто передадите пустую функцию.
+//onItemClicked принимает данные Entity
+class NotesAdapter(private val onItemClicked: (NoteEntity) -> Unit) :
+    ListAdapter<NoteEntity, NotesAdapter.ViewHolder>(DiffCallback) {
+
+    class ViewHolder(private var binding: ItemNoteBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        //функция для установки названия продукта, цены и количества на складе
+        fun bind(itemEntity: NoteEntity) {
+            binding.apply {
+                titleTextView.text = itemEntity.title
+                //Получите цену в денежном формате с помощью getFormattedPrice() функции расширения
+                descriptionTextView.text = itemEntity.description
+                dateOfCreation.text = itemEntity.getFormattedData()
+            }
+        }
+
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
         return ViewHolder(
-            FragmentNotesListBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
+            ItemNoteBinding.inflate(
+                LayoutInflater.from(parent.context)
             )
         )
 
     }
 
+    /**
+    Заменяет старые данные существующей View новыми данными с пом метода bind
+    //Получите текущий элемент, используя метод getItem(), передав позицию.
+     */
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = values[position]
-
+        val current = getItem(position)
+        //слушатель для вызова onItemClicked() элемента в текущей позиции
+        holder.itemView.setOnClickListener {
+            onItemClicked(current)
+        }
+        holder.bind(current)
     }
 
-    override fun getItemCount(): Int = values.size
+    companion object {
+        //DiffCallback класс, который вы указали для ListAdapter? Это просто объект,
+        //который помогает ListAdapter определить, какие элементы в новом и старом списках отличаются при обновлении списка.
+        private val DiffCallback = object : DiffUtil.ItemCallback<NoteEntity>() {
 
-    inner class ViewHolder(binding: FragmentNotesListBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+            override fun areItemsTheSame(
+                oldItemEntity: NoteEntity,
+                newItemEntity: NoteEntity
+            ): Boolean {
+                return oldItemEntity == newItemEntity
+            }
 
+            //проверка по названию продукта
+            override fun areContentsTheSame(
+                oldItemEntity: NoteEntity,
+                newItemEntity: NoteEntity
+            ): Boolean {
+                return oldItemEntity.title == newItemEntity.title
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/notes/view/NotesListFragment.kt
+++ app/src/main/java/com/example/notes/view/NotesListFragment.kt
@@ -5,9 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.notes.NotesAdapter
+import com.example.notes.NotesViewModel
+import com.example.notes.NotesViewModelFactory
 import com.example.notes.R
 import com.example.notes.databinding.FragmentNotesListBinding
+import com.example.notes.room.NotesApplication
 
 /**
  * A fragment representing a list of Items.
@@ -17,18 +23,42 @@ class NotesListFragment : Fragment() {
     private var _binding: FragmentNotesListBinding? = null
     private val binding get() = _binding!!
 
+    //привязываем viewModel с методами Dao к нашему фрагменту со списком заметок
+    private val viewModel: NotesViewModel by activityViewModels {
+        // про InventoryViewModelFactory см. BusScheduler 5/1/2/6
+        NotesViewModelFactory(
+            (activity?.application as NotesApplication).database.noteDao()
+        )
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentNotesListBinding.inflate(inflater, container,false)
+        _binding = FragmentNotesListBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //Добавили обработчик щелчка к RecyclerView для навигации в к Item Details
+        val adapter = NotesAdapter {
+        }
+
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = GridLayoutManager(this.context, 2)
+
+        //Прикрепим наблюдателя observe к объекту allItems, чтобы отслеживать изменения данных.
+        //allItems - получить все столбцы таблицы.
+        //Внутри наблюдателя observe, вызоваем submitList() и передаем новый список с обновленными данными.
+        viewModel.allItems.observe(this.viewLifecycleOwner) { items ->
+            items.let {
+                adapter.submitList(it)
+            }
+        }
 
         binding.createANoteButton.setOnClickListener {
             val action = NotesListFragmentDirections.actionNotesListFragmentToAddItemFragment4(
